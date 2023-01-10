4b2bdd73aaf17775e73d19fe2d307b4f064538aa
hspark <ssook7979@gmail.com>
2023-01-10 18:48:19 +0900

    init

---
+++ __tests__/UserRegister.spec.js
@@ -0,0 +1,77 @@
+const request = require('supertest');
+const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(() => {
+  return sequelize.sync();
+});
+
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
+
+describe('User Registration', () => {
+  it('returns 200 OK when signup request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
+
+  it('returns success message when signup is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.body.message).toBe('User created');
+        done();
+      });
+  });
+
+  it('saves the user to database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
+      });
+  });
+
+  it('saves the username and email to database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          const savedUser = userList[0];
+          expect(userList.length).toBe(1);
+          expect(savedUser.username).toBe('user1');
+          expect(savedUser.email).toBe('user1@mail.com');
+          done();
+        });
+      });
+  });
+});
