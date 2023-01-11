9c8c13618c493ef8a988c60824a762b0dc55de6d
hspark <ssook7979@gmail.com>
2023-01-11 12:22:18 +0900

    refactor code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const User = require('./user/User');
+const UserRouter = require('./user/UserRouter');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/api/1.0/users', (req, res) => {
-  User.create(req.body).then(() => {
-    return res.send({ message: 'User created' });
-  });
-});
+app.use(UserRouter);
+
+console.log('env: ' + process.env.NODE_ENV);
 
 module.exports = app;
