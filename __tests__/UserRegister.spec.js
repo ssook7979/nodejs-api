f0e9b60d5964d2562fb3f1147ef755ee57d6421a
hspark <ssook7979@gmail.com>
2023-01-11 12:29:45 +0900

    refactored postValidUser method to post both valid and invalid user

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -12,31 +12,32 @@ beforeEach(() => {
 });
 
 describe('User Registration', () => {
-  const postValidUser = () => {
-    return request(app).post('/api/1.0/users').send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'P4ssword',
-    });
+  const validUser = {
+    username: 'user1',
+    email: 'user1@mail.com',
+    password: 'P4ssword',
+  };
+  const postUser = (user = validUser) => {
+    return request(app).post('/api/1.0/users').send(user);
   };
   it('returns 200 OK when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.status).toBe(200);
   });
 
   it('returns success message when signup is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.body.message).toBe('User created');
   });
 
   it('saves the user to database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     expect(userList.length).toBe(1);
   });
 
   it('saves the username and email to database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     const savedUser = userList[0];
     expect(userList.length).toBe(1);
@@ -45,10 +46,19 @@ describe('User Registration', () => {
   });
 
   it('hashes the password in database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     const savedUser = userList[0];
     expect(userList.length).toBe(1);
     expect(savedUser.password).not.toBe('P4ssword');
+  });
+
+  it('returns 400 when username is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    });
+    expect(response.status).toBe(400);
   });
 });
