87cd9e5dc2beb547acebc01be8f9ce6f0f876103
hspark <ssook7979@gmail.com>
2023-01-16 14:07:24 +0900

    use locales for error messages

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -7,32 +7,29 @@ router.post(
   '/api/1.0/users',
   check('username')
     .notEmpty()
-    .withMessage('Username cannot be null.')
+    .withMessage('username_null')
     .bail()
     .isLength({ min: 4 })
-    .withMessage('Must have min 4 and max 32 characters.'),
+    .withMessage('username_size'),
   check('password')
     .notEmpty()
-    .withMessage('Password cannot be null.')
+    .withMessage('password_null')
     .bail()
     .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters.')
+    .withMessage('password_size')
     .bail()
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
-    .withMessage(
-      'Password must have at least 1 uppercase, 1 lowercase and 1 number.'
-    ),
+    .withMessage('password_invalid'),
   check('email')
     .notEmpty()
-    .withMessage('Email cannot be null.')
+    .withMessage('email_null')
     .bail()
     .isEmail()
-    .withMessage('Email is not valid.')
+    .withMessage('email_invalid')
     .bail()
     .custom(async (email) => {
       const user = await UserService.findByEmail(email);
       if (user) {
-        console.log(user);
         throw new Error('Email in use.');
       }
       return true;
@@ -42,7 +39,7 @@ router.post(
     if (!errors.isEmpty()) {
       const validationErrors = {};
       errors.array().forEach((error) => {
-        validationErrors[error.param] = error.msg;
+        validationErrors[error.param] = req.t(error.msg);
       });
       return res.status(400).send({ validationErrors });
     }
