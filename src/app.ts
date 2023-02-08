d2dc2007fbfd587662c3a8b5a40e1419260814cd
hspark <ssook7979@gmail.com>
2023-02-08 12:35:02 +0900

    to serve static files

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { json } from 'express';
 import { handle } from 'i18next-http-middleware';
+import config from 'config';
+import * as path from 'path';
 import UserRouter from './user/UserRouter';
 import AuthenticationRouter from './auth/AuthenticationRouter';
 import ErrorHandler from './error/ErrorHandler';
 import i18next from './config/i18next';
 import tokenAuthentication from './middleware/tokenAutentication';
 import * as FileService from '../src/file/FileService';
+
+const uploadDir: string = config.get('uploadDir');
+const profileDir: string = config.get('profileDir');
+const profileFolder: string = path.join('.', uploadDir, profileDir);
+
+const ONE_YEAR_IN_MILLI = 365 * 24 * 60 * 60 * 1000;
 
 FileService.createFolders();
 
@@ -14,6 +22,11 @@ const app = express();
 app.use(handle(i18next));
 
 app.use(json());
+
+app.use(
+  '/images',
+  express.static(profileFolder, { maxAge: ONE_YEAR_IN_MILLI })
+);
 
 app.use(tokenAuthentication);
 
