de31086e6bedbfb3e1d6fba42e2edba23db28579
hspark <ssook7979@gmail.com>
2023-02-13 19:05:49 +0900

    added test case for posting hox without auth

---
+++ __tests__/HoaxSubmit.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test, beforeAll, beforeEach } from '@jest/globals';
+import request from 'supertest';
+import app from '../src/app';
+
+const postHoax = (body?: any) => {
+  return request(app).post('/api/1.0/hoaxes').send(body);
+};
+
+describe('Post hoax', () => {
+  test('returns 401 when hoax post request has no authentication', async () => {
+    const response = await postHoax();
+    expect(response.status).toBe(401);
+  });
+});
